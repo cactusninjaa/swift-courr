6ab9c2ad5f92242c3047a2494755074d111b765c
Thomas Sauvage <thomas.sauvage@edu.devinci.fr>
2025-11-03 09:15:18 +0100

    Initial Commit

---
+++ cour/courApp.swift
@@ -0,0 +1,17 @@
+//
+//  courApp.swift
+//  cour
+//
+//  Created by thomas sauvage on 03/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct courApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ courTests/courTests.swift
@@ -0,0 +1,16 @@
+//
+//  courTests.swift
+//  courTests
+//
+//  Created by thomas sauvage on 03/11/2025.
+//
+
+import Testing
+
+struct courTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
